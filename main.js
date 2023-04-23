1815b885521f376f320da04bc10f43e26a5bc60a
ursha <benas.pekarskis@gmail.com>
2023-04-23 12:00:19 +0100

    added bootstrap sidebar and basemap

--- main.js
+++ main.js
@@ -2,17 +2,36 @@ import './style.css';
 import {Map, View} from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
+import XYZ from 'ol/source/XYZ';
 import {fromLonLat} from 'ol/proj';
+
+const osmLayer = new TileLayer({
+  source: new OSM()
+});
+
+const satelliteLayer = new TileLayer({
+  source: new XYZ({
+    url: 'https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',
+    attributions: 'Map data ©2022 Google'
+  })
+});
+
 const map = new Map({
   target: 'map',
-  layers: [
-    new TileLayer({
-      source: new OSM()
-    })
-  ],
+  layers: [osmLayer],
   view: new View({
-    // Change the center property to use Lithuania's coordinates
     center: fromLonLat([24.6, 55.7]),
     zoom: 7
   })
+});
+
+document.getElementById('basemap-select').addEventListener('change', event => {
+  const value = event.target.value;
+  if (value === 'osm') {
+    map.removeLayer(satelliteLayer);
+    map.addLayer(osmLayer);
+  } else if (value === 'satellite') {
+    map.removeLayer(osmLayer);
+    map.addLayer(satelliteLayer);
+  }
 });
